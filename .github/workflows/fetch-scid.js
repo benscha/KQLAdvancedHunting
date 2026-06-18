5f050d87ee35cd4ca1d14eb3abc2259c4536c646
Benjamin Zulliger <38983142+benscha@users.noreply.github.com>
2026-06-18 16:00:40 +0200

    Update fetch-scid.js

--- .github/workflows/fetch-scid.js
+++ .github/workflows/fetch-scid.js
@@ -1,4 +1,3 @@
-// fetch-scid.js
 const { chromium } = require('playwright');
 const fs = require('fs');
 const path = require('path');
@@ -7,20 +6,31 @@ const path = require('path');
 	const browser = await chromium.launch();
 	const page = await browser.newPage();
 
-	// CSV-Download abfangen
 	const [download] = await Promise.all([
 		page.waitForEvent('download'),
 		(async () => {
 			await page.goto('https://docs.kaidojarvemets.com/defender-scid-explorer.html');
-			// Warten bis Daten geladen
-			await page.waitForSelector('text=Export as CSV', { timeout: 10000 });
+			await page.waitForSelector('text=Export as CSV', { timeout: 15000 });
 			await page.click('text=Export as CSV');
 		})()
 	]);
 
 	const csvPath = path.join('MISC', 'scid.csv');
 	fs.mkdirSync('MISC', { recursive: true });
-	await download.saveAs(csvPath);
+
+	// Temporär speichern
+	const tmpPath = path.join('MISC', 'scid_tmp.csv');
+	await download.saveAs(tmpPath);
+
+	// Kommentar-Header voranstellen
+	const originalContent = fs.readFileSync(tmpPath, 'utf8');
+	const header = [
+		'# Thx to Kaido Järvemets for the Defender Scid Explorer',
+		'# SRC: https://docs.kaidojarvemets.com/defender-scid-explorer',
+		''
+	].join('\n');
+	fs.writeFileSync(csvPath, header + originalContent, 'utf8');
+	fs.unlinkSync(tmpPath);
 
 	console.log('CSV saved to', csvPath);
 	await browser.close();
